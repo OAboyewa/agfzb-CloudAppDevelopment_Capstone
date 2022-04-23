ec5c93f6f0e2d74ea353aa22f1b99719cd028460
OAboyewa <oluwatobiaboyewa@gmail.com>
2022-04-23 15:49:11 +0000

    created models carmake and carmodel

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,7 +9,13 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=30)
+    description = models.CharField(null=False, max_length=30)
 
+    # Create a toString method for object string representation
+    def __str__(self):
+        return self.name + " " + self.description
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -19,7 +25,31 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    SPORTS = 'Sports'
+    SEDAN = 'Sedan'
+    WAGON = 'Wagon'
+    SUV = 'Suv'
+    COUPE = 'Coupe'
 
+    carChoices = [
+        (SPORTS, 'Sports'),
+        (SEDAN, 'Sedan'),
+        (WAGON, 'Wagon'), 
+        (SUV, 'Suv'), 
+        (COUPE, 'Coupe')
+    ]
+
+    car_make = models.ForeignKey(CarMake, null=False, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=30)
+    id = models.IntegerField(default=0, primary_key=True)
+    car_model = models.CharField(null=False, max_length=30, choices=carChoices)
+    car_year = models.DateField(null=False)
+
+    # Create a toString method for object string representation
+    def __str__(self):
+        return (self.car_make + " " + self.name + " " + self.id +
+         " " + self.car_model +  " " + self.car_year)
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
 
